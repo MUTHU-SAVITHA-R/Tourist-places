38fcaf677da3e98d2d01301d88c252f537c48069
Muthu Savitha R <muthusavithar@gmail.com>
2026-01-04 00:36:24 +0530

    updated

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -73,7 +73,7 @@ function Navbar(props) {
         <div style={styles.leftGroup}>
           <h4 style={styles.h4} onClick={goHome}>Home </h4>
           <h4 style={styles.h4} onClick={scrollToAbout}>About</h4>
-          {!props.setIsLoggedIn &&(
+          {!props.isLoggedIn &&(
             <>
           <button style={styles.button} onClick={gologin}>Login</button>
           <button style={styles.button}>Signup</button>
