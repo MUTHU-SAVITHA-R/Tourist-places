deb3fb78b81bd6d072cec0b95862e3b53f48a37a
Muthu Savitha R <muthusavithar@gmail.com>
2026-01-11 12:26:53 +0530

    updated

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -3,14 +3,16 @@ import tour from "./assets/tour.jpg";
 import TouristExperience from "./TouristExperience";
 
 function Homepage() {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const styles = {
     container: {
       textAlign: "center",
-      marginTop: "0px", 
+      marginTop: "0px",
       background: "linear-gradient(135deg, #013A63, #89CFF0)",
-      minHeight: "calc(100vh - 80px)", 
+      minHeight: "100vh",
+      overflowX: "hidden",
+      paddingBottom: "40px"
     },
     cityBar: {
       background: "#0A2A43",
@@ -39,7 +41,7 @@ function Homepage() {
         <p onClick={() => navigate("/chennai")}>Chennai</p>
         <p onClick={() => navigate("/kanyakumari")}>Kanyakumari</p>
       </div>
-      
+
 
       <img style={styles.img} src={tour} alt="tour" />
 
